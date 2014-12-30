81c8c5f34627a63b010fbfc747edd5aeb5b2a658
= <kaven.feng@autodesk.com>
2014-12-30 11:09:30 +0800

    new file for T3

---
+++ TestConcurency11/T3.cpp
@@ -0,0 +1,13 @@
+//
+//  T3.cpp
+//  TestConcurency11
+//
+//  Created by Kaven Feng on 12/30/14.
+//  Copyright (c) 2014 Kaven Feng. All rights reserved.
+//
+
+#include "T3.h"
+
+namespace T30 {
+    
+}
---
+++ TestConcurency11/T3.h
@@ -0,0 +1,18 @@
+//
+//  T3.h
+//  TestConcurency11
+//
+//  Created by Kaven Feng on 12/30/14.
+//  Copyright (c) 2014 Kaven Feng. All rights reserved.
+//
+
+#ifndef __TestConcurency11__T3__
+#define __TestConcurency11__T3__
+
+#include <iostream>
+
+namespace T30 {
+    void run();
+}
+
+#endif /* defined(__TestConcurency11__T3__) */
